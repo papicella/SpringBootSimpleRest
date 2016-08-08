a46dc03e5fa2f2547c1dec0d3ba052f6aa239ee3
papicella <pas.apicella@tpg.com.au>
2016-08-08 20:39:04 +1000

    Initial commit

---
+++ src/main/java/com/example/SpringBootSimpleRestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleRestApplication.class, args);
+	}
+}
